070e9acd880ac118c365b7891694f93119a30e61
Jose Herrada <jherrada@wcloudservices.com>
2021-10-05 17:55:00 -0400

    Colore de sidebar

--- src/core/shared/sidebar/SideBar.js
+++ src/core/shared/sidebar/SideBar.js
@@ -9,9 +9,6 @@ class SideBar extends Component {
             <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                    <div className="sidebar-brand-icon rotate-n-15">
-                        <i className="fas fa-laugh-wink"></i>
-                    </div>
                     <div className="sidebar-brand-text mx-3">Github</div>
                 </a>
 
